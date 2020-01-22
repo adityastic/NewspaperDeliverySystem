5fd77d8caa1e1a05a2c428d2914c91f0a99ca850
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-01-22 09:28:31 +0000

    chore: added spring boot generated project

---
+++ src/main/java/com/laqr/NewspaperDeliverySystem/NewspaperDeliverySystemApplication.java
@@ -0,0 +1,13 @@
+package com.laqr.NewspaperDeliverySystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspaperDeliverySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewspaperDeliverySystemApplication.class, args);
+	}
+
+}
