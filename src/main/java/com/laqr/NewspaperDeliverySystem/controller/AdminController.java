7034e6ae42887530b868dc0b4ee3ac91acf2cbea
Lorraine Armitage <lorrainearmitageait@gmail.com>
2020-02-11 16:14:37 +0000

    added logic for admin

---
+++ src/main/java/com/laqr/NewspaperDeliverySystem/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.laqr.NewspaperDeliverySystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin/home")
+    public String adminHomePage(){
+        return "admin-home";
+    }
+}
