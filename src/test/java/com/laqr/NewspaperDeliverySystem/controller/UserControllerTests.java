f2c42e51c6b69271565d9f8e7642c3b2f13752ba
Rupal Singh <levelzerodev@gmail.com>
2020-02-12 12:05:38 +0000

    feat: add home end point for user

--- src/test/java/com/laqr/NewspaperDeliverySystem/controller/UserControllerTests.java
+++ src/test/java/com/laqr/NewspaperDeliverySystem/controller/UserControllerTests.java
@@ -4,11 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @SpringBootTest
