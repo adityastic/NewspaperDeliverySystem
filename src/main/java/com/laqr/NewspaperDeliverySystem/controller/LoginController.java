03237f8db819958b0b711942de5f63a7484b17a1
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-02-16 12:57:43 +0000

    chore: added session attribute to save user details

--- src/main/java/com/laqr/NewspaperDeliverySystem/controller/LoginController.java
+++ src/main/java/com/laqr/NewspaperDeliverySystem/controller/LoginController.java
@@ -3,7 +3,9 @@ package com.laqr.NewspaperDeliverySystem.controller;
 import com.laqr.NewspaperDeliverySystem.services.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -53,11 +55,20 @@ public class LoginController {
             return new ModelAndView("redirect:/admin/home");
         } else if (loginService.checkUser(username, password)) {
             session.setAttribute("username", username);
-            session.setAttribute("password", username);
+            session.setAttribute("password", password);
             return new ModelAndView("redirect:/home");
         } else {
             redirectAttributes.addFlashAttribute("error", "username and password is incorrect");
             return new ModelAndView("redirect:/");
         }
+    }
+
+
+    @GetMapping(value = "/logout")
+    public String logout(
+            HttpSession session
+    ) {
+        session.invalidate();
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/laqr/NewspaperDeliverySystem/services/UserService.java
@@ -0,0 +1,27 @@
+package com.laqr.NewspaperDeliverySystem.services;
+
+import com.laqr.NewspaperDeliverySystem.model.User;
+import com.laqr.NewspaperDeliverySystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public boolean checkUser(String username, String password) {
+        Optional<User> maybeUser = userRepository.findByUsername(username);
+        return maybeUser.isPresent() && maybeUser.get().getPassword().equals(password);
+    }
+
+    public User getUser(String username, String password) {
+        if(!checkUser(username,password)){
+            return null;
+        }
+        return userRepository.findByUsername(username).get();
+    }
+}
