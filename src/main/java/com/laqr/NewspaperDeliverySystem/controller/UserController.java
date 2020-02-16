8247b613a182ec88159fad031623f0aab6dc967a
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-02-16 14:11:46 +0000

    feat: added session attribute to save user details (#43)

--- src/main/java/com/laqr/NewspaperDeliverySystem/controller/UserController.java
+++ src/main/java/com/laqr/NewspaperDeliverySystem/controller/UserController.java
@@ -1,13 +1,34 @@
 package com.laqr.NewspaperDeliverySystem.controller;
 
+import com.laqr.NewspaperDeliverySystem.model.User;
+import com.laqr.NewspaperDeliverySystem.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class UserController {
 
-    @GetMapping( "/home")
-    public String firstPage(ModelMap model) {
-        return "user/home";
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/home")
+    public String firstPage(
+            ModelMap model,
+            HttpSession session
+    ) {
+        String username = (String) session.getAttribute("username");
+        String password = (String) session.getAttribute("password");
+
+        User currentUser = userService.getUser(username, password);
+        if (currentUser != null) {
+            model.addAttribute(currentUser);
+            return "user/home";
+        } else {
+            return "redirect:/";
+        }
     }
 }
