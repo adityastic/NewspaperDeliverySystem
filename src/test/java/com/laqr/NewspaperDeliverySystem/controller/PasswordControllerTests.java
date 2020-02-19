10b955b51ff411a77e6aad366ef8ec61b3c70682
Lorraine Armitage <lorrainearmitageait@gmail.com>
2020-02-19 14:48:18 +0000

    feat: forgot password controller

---
+++ src/test/java/com/laqr/NewspaperDeliverySystem/controller/PasswordControllerTests.java
@@ -0,0 +1,42 @@
+package com.laqr.NewspaperDeliverySystem.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PasswordControllerTests {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    @Sql(scripts = "/scripts/controller/users_before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = "/scripts/controller/users_after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    void testChangePasswordSuccess() throws Exception {
+        mvc.perform(post("/forgotPassword")
+                .param("username", "lorraine")
+                .param("password", "lorraine"))
+                .andDo(print())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(flash().attributeExists("success"));
+    }
+
+    @Test
+    void testChangePasswordError() throws Exception {
+        mvc.perform(post("/forgotPassword")
+                .param("username", "asd")
+                .param("password", "asdas"))
+                .andExpect(redirectedUrl("/forgotPassword"))
+                .andExpect(flash().attributeExists("error"));
+    }
+}
