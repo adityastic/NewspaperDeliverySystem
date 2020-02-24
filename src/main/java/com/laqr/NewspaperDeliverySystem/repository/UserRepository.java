c93a83e03df5b2efa1f7ff7206d0e091cbede7a6
Rupal Singh <43191803+rupalsing@users.noreply.github.com>
2020-02-24 17:13:30 +0000

    chore: updated user repository (#51)
    Co-authored-by: Aditya Gupta <adityaofficialgupta@gmail.com>

--- src/main/java/com/laqr/NewspaperDeliverySystem/repository/UserRepository.java
+++ src/main/java/com/laqr/NewspaperDeliverySystem/repository/UserRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByUsernameAndPassword(String username, String password);
 }
