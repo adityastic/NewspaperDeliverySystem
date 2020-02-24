ffef6c3ea6068170d136747f8b91e94c05db87f5
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-02-24 21:28:59 +0000

    r

--- src/main/java/com/laqr/NewspaperDeliverySystem/repository/DeliveryPersonRepository.java
+++ src/main/java/com/laqr/NewspaperDeliverySystem/repository/DeliveryPersonRepository.java
@@ -1,10 +1,8 @@
 package com.laqr.NewspaperDeliverySystem.repository;
 
 import com.laqr.NewspaperDeliverySystem.model.DeliveryPerson;
-import com.laqr.NewspaperDeliverySystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface DeliveryPersonRepository extends JpaRepository<DeliveryPerson, Integer> {
 }
