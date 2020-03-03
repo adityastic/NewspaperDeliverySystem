3645439190aeb6da5c2edacf21c8cd0c181b5e04
qianrou2020 <60349736+qianrou2020@users.noreply.github.com>
2020-03-03 12:19:57 +0000

    feat: add addroute test (#84)
    Co-authored-by: lorrainearm <60215590+lorrainearm@users.noreply.github.com>

--- src/test/java/com/laqr/NewspaperDeliverySystem/controller/route/RouteControllerTests.java
+++ src/test/java/com/laqr/NewspaperDeliverySystem/controller/route/RouteControllerTests.java
@@ -7,10 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,9 +19,40 @@ public class RouteControllerTests {
     MockMvc mvc;
 
     @Test
+    void addRouteGetTest() throws Exception {
+        mvc.perform(get("/admin/add-route")
+                .sessionAttr("username", "admin")
+                .sessionAttr("password", "admin"))
+                .andDo(print())
+                .andExpect(view().name("admin/route/add"));
+    }
+
+    @Test
+    void addRoutePostTest() throws Exception {
+        mvc.perform(post("/admin/add-route")
+                .param("route-name", "Dublin")
+                .sessionAttr("username", "admin")
+                .sessionAttr("password", "admin"))
+                .andDo(print())
+                .andExpect(redirectedUrl("/admin/view-route"))
+                .andExpect(flash().attribute("success", "Successfully Added Route"));
+    }
+
+    @Test
     @Sql(scripts = "/scripts/controller/routes_before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/scripts/controller/routes_after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-    void routeValid() throws Exception {
+    void editRouteGetTest() throws Exception {
+        mvc.perform(get("/admin/edit-route/1")
+                .sessionAttr("username", "admin")
+                .sessionAttr("password", "admin"))
+                .andDo(print())
+                .andExpect(view().name("admin/route/edit"));
+    }
+
+    @Test
+    @Sql(scripts = "/scripts/controller/routes_before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = "/scripts/controller/routes_after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    void editRoutePostTest() throws Exception {
         mvc.perform(post("/admin/edit-route")
                 .param("route-id","1")
                 .param("route-name", "Dublin")
