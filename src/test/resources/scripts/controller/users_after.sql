72783a25554234c2552f71aee0f46a7c66d4825e
Rupal Singh <levelzerodev@gmail.com>
2020-03-04 15:52:22 +0000

    feat: updated delivery person

--- src/test/resources/scripts/controller/users_after.sql
+++ src/test/resources/scripts/controller/users_after.sql
@@ -1,3 +1,5 @@
 DELETE FROM user_details WHERE id = 100;
 DELETE FROM user_details WHERE id = 50;
 DELETE FROM user_details WHERE username = 'adityagupta';
+DELETE FROM delivery_person WHERE route = 10;
+DELETE FROM route WHERE id = 10;
