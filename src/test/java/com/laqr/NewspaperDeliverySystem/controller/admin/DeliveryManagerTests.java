909be1357c78b01001dc28f87c66e2091a11b2e2
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-03-10 16:38:10 +0000

    r

--- src/test/java/com/laqr/NewspaperDeliverySystem/controller/admin/DeliveryManagerTests.java
+++ src/test/java/com/laqr/NewspaperDeliverySystem/controller/admin/DeliveryManagerTests.java
@@ -23,7 +23,7 @@ public class DeliveryManagerTests {
     @Sql(scripts = "/scripts/controller/users_before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/scripts/controller/users_after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void usernameAlreadyExists() throws Exception {
-        mvc.perform(post("/admin/register-delivery-persons")
+        mvc.perform(post("/admin/add-delivery-persons")
                 .param("username", "lorraine")
                 .param("password", "athlone")
                 .param("full-name", "Lorraine Armitage")
@@ -32,7 +32,7 @@ public class DeliveryManagerTests {
                 .sessionAttr("username", "admin")
                 .sessionAttr("password", "admin"))
                 .andDo(print())
-                .andExpect(redirectedUrl("/admin/register-delivery-persons"))
+                .andExpect(redirectedUrl("/admin/add-delivery-persons"))
                 .andExpect(flash().attribute("error", "Username already exists"));
     }
 
@@ -40,7 +40,7 @@ public class DeliveryManagerTests {
     @Sql(scripts = "/scripts/controller/users_before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/scripts/controller/users_after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void usernameNotEntered() throws Exception {
-        mvc.perform(post("/admin/register-delivery-persons")
+        mvc.perform(post("/admin/add-delivery-persons")
                 .param("username", "")
                 .param("password", "athlone")
                 .param("full-name", "Lorraine Armitage")
@@ -49,7 +49,7 @@ public class DeliveryManagerTests {
                 .sessionAttr("username", "admin")
                 .sessionAttr("password", "admin"))
                 .andDo(print())
-                .andExpect(redirectedUrl("/admin/register-delivery-persons"))
+                .andExpect(redirectedUrl("/admin/add-delivery-persons"))
                 .andExpect(flash().attribute("error", "No Username Entered"));
     }
 
@@ -57,7 +57,7 @@ public class DeliveryManagerTests {
     @Sql(scripts = "/scripts/controller/users_before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/scripts/controller/users_after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void usernameLengthLess4() throws Exception {
-        mvc.perform(post("/admin/register-delivery-persons")
+        mvc.perform(post("/admin/add-delivery-persons")
                 .param("username", "lor")
                 .param("password", "athlone")
                 .param("full-name", "Lorraine Armitage")
@@ -66,7 +66,7 @@ public class DeliveryManagerTests {
                 .sessionAttr("username", "admin")
                 .sessionAttr("password", "admin"))
                 .andDo(print())
-                .andExpect(redirectedUrl("/admin/register-delivery-persons"))
+                .andExpect(redirectedUrl("/admin/add-delivery-persons"))
                 .andExpect(flash().attribute("error", "Username should be from length 4 - 15"));
     }
 
@@ -74,7 +74,7 @@ public class DeliveryManagerTests {
     @Sql(scripts = "/scripts/controller/users_before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/scripts/controller/users_after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void usernameLengthGreat15() throws Exception {
-        mvc.perform(post("/admin/register-delivery-persons")
+        mvc.perform(post("/admin/add-delivery-persons")
                 .param("username", "lorraineasdasdasdasd")
                 .param("password", "athlone")
                 .param("full-name", "Lorraine Armitage")
@@ -83,7 +83,7 @@ public class DeliveryManagerTests {
                 .sessionAttr("username", "admin")
                 .sessionAttr("password", "admin"))
                 .andDo(print())
-                .andExpect(redirectedUrl("/admin/register-delivery-persons"))
+                .andExpect(redirectedUrl("/admin/add-delivery-persons"))
                 .andExpect(flash().attribute("error", "Username should be from length 4 - 15"));
     }
 
@@ -91,7 +91,7 @@ public class DeliveryManagerTests {
     @Sql(scripts = "/scripts/controller/users_before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/scripts/controller/users_after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void usernameSpecialChar() throws Exception {
-        mvc.perform(post("/admin/register-delivery-persons")
+        mvc.perform(post("/admin/add-delivery-persons")
                 .param("username", "lorrai@as")
                 .param("password", "athlone")
                 .param("full-name", "Lorraine Armitage")
@@ -100,7 +100,7 @@ public class DeliveryManagerTests {
                 .sessionAttr("username", "admin")
                 .sessionAttr("password", "admin"))
                 .andDo(print())
-                .andExpect(redirectedUrl("/admin/register-delivery-persons"))
+                .andExpect(redirectedUrl("/admin/add-delivery-persons"))
                 .andExpect(flash().attribute("error", "Username should not contain any special characters"));
     }
 
@@ -108,7 +108,7 @@ public class DeliveryManagerTests {
     @Sql(scripts = "/scripts/controller/users_before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/scripts/controller/users_after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void passwordNotEntered() throws Exception {
-        mvc.perform(post("/admin/register-delivery-persons")
+        mvc.perform(post("/admin/add-delivery-persons")
                 .param("username", "lorraines")
                 .param("password", "")
                 .param("full-name", "Lorraine Armitage")
@@ -117,7 +117,7 @@ public class DeliveryManagerTests {
                 .sessionAttr("username", "admin")
                 .sessionAttr("password", "admin"))
                 .andDo(print())
-                .andExpect(redirectedUrl("/admin/register-delivery-persons"))
+                .andExpect(redirectedUrl("/admin/add-delivery-persons"))
                 .andExpect(flash().attribute("error", "No Password Entered"));
     }
 
@@ -125,7 +125,7 @@ public class DeliveryManagerTests {
     @Sql(scripts = "/scripts/controller/users_before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/scripts/controller/users_after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void passwordLength() throws Exception {
-        mvc.perform(post("/admin/register-delivery-persons")
+        mvc.perform(post("/admin/add-delivery-persons")
                 .param("username", "lorraines")
                 .param("password", "123")
                 .param("full-name", "Lorraine Armitage")
@@ -134,7 +134,7 @@ public class DeliveryManagerTests {
                 .sessionAttr("username", "admin")
                 .sessionAttr("password", "admin"))
                 .andDo(print())
-                .andExpect(redirectedUrl("/admin/register-delivery-persons"))
+                .andExpect(redirectedUrl("/admin/add-delivery-persons"))
                 .andExpect(flash().attribute("error", "Password should be at least 4"));
     }
 
@@ -142,7 +142,7 @@ public class DeliveryManagerTests {
     @Sql(scripts = "/scripts/controller/users_before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/scripts/controller/users_after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void phoneNoNotEntered() throws Exception {
-        mvc.perform(post("/admin/register-delivery-persons")
+        mvc.perform(post("/admin/add-delivery-persons")
                 .param("username", "lorraines")
                 .param("password", "1234")
                 .param("full-name", "Lorraine Armitage")
@@ -151,7 +151,7 @@ public class DeliveryManagerTests {
                 .sessionAttr("username", "admin")
                 .sessionAttr("password", "admin"))
                 .andDo(print())
-                .andExpect(redirectedUrl("/admin/register-delivery-persons"))
+                .andExpect(redirectedUrl("/admin/add-delivery-persons"))
                 .andExpect(flash().attribute("error", "No Phone Number Entered"));
     }
 
@@ -159,7 +159,7 @@ public class DeliveryManagerTests {
     @Sql(scripts = "/scripts/controller/users_before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/scripts/controller/users_after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void phoneNoLength() throws Exception {
-        mvc.perform(post("/admin/register-delivery-persons")
+        mvc.perform(post("/admin/add-delivery-persons")
                 .param("username", "lorrainess")
                 .param("password", "1234")
                 .param("full-name", "Lorraine Armitage")
@@ -168,7 +168,7 @@ public class DeliveryManagerTests {
                 .sessionAttr("username", "admin")
                 .sessionAttr("password", "admin"))
                 .andDo(print())
-                .andExpect(redirectedUrl("/admin/register-delivery-persons"))
+                .andExpect(redirectedUrl("/admin/add-delivery-persons"))
                 .andExpect(flash().attribute("error", "Phone Number should be of 10 numbers"));
     }
 
@@ -176,7 +176,7 @@ public class DeliveryManagerTests {
     @Sql(scripts = "/scripts/controller/users_before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/scripts/controller/users_after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void fullNameNotEntered() throws Exception {
-        mvc.perform(post("/admin/register-delivery-persons")
+        mvc.perform(post("/admin/add-delivery-persons")
                 .param("username", "lorraines")
                 .param("password", "1234")
                 .param("full-name", "")
@@ -185,7 +185,7 @@ public class DeliveryManagerTests {
                 .sessionAttr("username", "admin")
                 .sessionAttr("password", "admin"))
                 .andDo(print())
-                .andExpect(redirectedUrl("/admin/register-delivery-persons"))
+                .andExpect(redirectedUrl("/admin/add-delivery-persons"))
                 .andExpect(flash().attribute("error", "No Full Name Entered"));
     }
 
@@ -193,7 +193,7 @@ public class DeliveryManagerTests {
     @Sql(scripts = "/scripts/controller/users_before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/scripts/controller/users_after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void detailsCorrect() throws Exception {
-        mvc.perform(post("/admin/register-delivery-persons")
+        mvc.perform(post("/admin/add-delivery-persons")
                 .param("username", "adityagupta")
                 .param("password", "123456")
                 .param("full-name", "Aditya Gupta")
@@ -202,7 +202,7 @@ public class DeliveryManagerTests {
                 .sessionAttr("username", "admin")
                 .sessionAttr("password", "admin"))
                 .andDo(print())
-                .andExpect(redirectedUrl("/admin/register-delivery-persons"))
+                .andExpect(redirectedUrl("/admin/view-delivery-persons"))
                 .andExpect(flash().attribute("success", "Added Delivery Person"));
     }
 
@@ -210,7 +210,7 @@ public class DeliveryManagerTests {
     @Sql(scripts = "/scripts/controller/users_before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/scripts/controller/users_after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void wrongUsernamePassword() throws Exception {
-        mvc.perform(post("/admin/register-delivery-persons")
+        mvc.perform(post("/admin/add-delivery-persons")
                 .param("username", "adityagupta")
                 .param("password", "123456")
                 .param("full-name", "Aditya Gupta")
@@ -226,7 +226,7 @@ public class DeliveryManagerTests {
     @Sql(scripts = "/scripts/controller/users_before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/scripts/controller/users_after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void getTest() throws Exception {
-        mvc.perform(get("/admin/register-delivery-persons")
+        mvc.perform(get("/admin/add-delivery-persons")
                 .sessionAttr("username", "admin")
                 .sessionAttr("password", "admin"))
                 .andExpect(view().name("admin/register"));
@@ -236,7 +236,7 @@ public class DeliveryManagerTests {
     @Sql(scripts = "/scripts/controller/users_before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(scripts = "/scripts/controller/users_after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void getTestWrongAdmin() throws Exception {
-        mvc.perform(get("/admin/register-delivery-persons")
+        mvc.perform(get("/admin/add-delivery-persons")
                 .sessionAttr("username", "admin")
                 .sessionAttr("password", ""))
                 .andExpect(redirectedUrl("/"));
