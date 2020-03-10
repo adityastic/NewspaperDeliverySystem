2253d01aacedcc12a2d9418ed3851ab692641098
Rupal Singh <43191803+rupalsing@users.noreply.github.com>
2020-03-10 21:46:09 +0000

    chore: added test for view product (#106)

--- src/test/java/com/laqr/NewspaperDeliverySystem/controller/admin/DeliveryPersonsTests.java
+++ src/test/java/com/laqr/NewspaperDeliverySystem/controller/admin/DeliveryPersonsTests.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class DeliveryManagerTests {
+public class DeliveryPersonsTests {
 
     @Autowired
     MockMvc mvc;
