906079b1a819dbce2f902ceccfa2079935943f50
Rupal Singh <levelzerodev@gmail.com>
2020-03-10 21:41:53 +0000

    chore: added test for view product

--- src/test/java/com/laqr/NewspaperDeliverySystem/controller/admin/RouteControllerTests.java
+++ src/test/java/com/laqr/NewspaperDeliverySystem/controller/admin/RouteControllerTests.java
@@ -1,4 +1,4 @@
-package com.laqr.NewspaperDeliverySystem.controller.admin.route;
+package com.laqr.NewspaperDeliverySystem.controller.admin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
