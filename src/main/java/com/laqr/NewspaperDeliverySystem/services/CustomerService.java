0515706c98d9fe99957bc2a18b8cc2e597a8991b
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-06-15 21:00:39 +0100

    feat: add customer completed (#140)

--- src/main/java/com/laqr/NewspaperDeliverySystem/services/CustomerService.java
+++ src/main/java/com/laqr/NewspaperDeliverySystem/services/CustomerService.java
@@ -2,6 +2,7 @@ package com.laqr.NewspaperDeliverySystem.services;
 
 import com.laqr.NewspaperDeliverySystem.model.Customer;
 import com.laqr.NewspaperDeliverySystem.repository.CustomerRepository;
+import com.laqr.NewspaperDeliverySystem.repository.RouteRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,12 +11,23 @@ import java.util.List;
 public class CustomerService {
 
     final CustomerRepository customerRepository;
+    final RouteRepository routeRepository;
 
-    public CustomerService(CustomerRepository customerRepository) {
+    public CustomerService(CustomerRepository customerRepository, RouteRepository routeRepository) {
         this.customerRepository = customerRepository;
+        this.routeRepository = routeRepository;
     }
 
     public List<Customer> getAllCustomers() {
         return customerRepository.findAll();
+    }
+
+    public void addCustomer(String fullName, String phoneNo, String address, List<Integer> subscriptions, Integer routeSelected) {
+        customerRepository.save(new Customer(fullName,
+                address,
+                Integer.parseInt(phoneNo),
+                subscriptions,
+                routeRepository.getOne(routeSelected),
+                null));
     }
 }
