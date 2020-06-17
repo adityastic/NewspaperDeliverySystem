f87048388af39070079f40c07fa1dbbb47001a4d
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-06-17 09:17:57 +0100

    feat: view customer tests

--- src/test/java/com/laqr/NewspaperDeliverySystem/controller/admin/CustomerControllerTests.java
+++ src/test/java/com/laqr/NewspaperDeliverySystem/controller/admin/CustomerControllerTests.java
@@ -8,7 +8,6 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
