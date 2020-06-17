83588bb3c27466795147621a9d52e1def20fd0ab
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-06-17 10:21:43 +0100

    feat: delete customer tests

--- src/test/resources/scripts/controller/customer_before.sql
+++ src/test/resources/scripts/controller/customer_before.sql
@@ -2,3 +2,5 @@ INSERT INTO products(id, buying_cost, day_of_week, frequency, name, selling_cost
     (10000, 1.2, null, 'DAILY', 'Athlone Daily', 1.4, 'NEWSPAPER'),
     (10001, 1.0, 5, 'WEEKLY', 'Athlone Weekly', 1.4, 'MAGAZINE');
 INSERT INTO route(id,name) VALUES (10000, 'Athlone');
+INSERT INTO newspaper_delivery.customers (id, address, full_name, holiday, phone_no, subscription, route) VALUES
+    (10001, 'ASDAASDSAD', 'Aditya', null, 1231231222, '[10000,10001]', 10000);
